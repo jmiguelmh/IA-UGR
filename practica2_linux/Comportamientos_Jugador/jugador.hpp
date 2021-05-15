448564e95e8038d3b241b115a97b21fac1648af9
Jose Miguel Marquez Herreros <jmiguelmh3000@gmail.com>
2021-05-15 17:09:42 +0200

    Práctica 2

--- practica2_linux/Comportamientos_Jugador/jugador.hpp
+++ practica2_linux/Comportamientos_Jugador/jugador.hpp
@@ -5,7 +5,8 @@
 
 #include <list>
 
-struct estado {
+struct estado
+{
   int fila;
   int columna;
   int orientacion;
@@ -13,49 +14,48 @@ struct estado {
   int costeUniforme;
 };
 
-class ComportamientoJugador : public Comportamiento {
-  public:
-    ComportamientoJugador(unsigned int size) : Comportamiento(size) {
-      // Inicializar Variables de Estado
-      hayPlan = false;
-      tieneBikini = false;
-      tieneZapatillas = false;
-    }
-    ComportamientoJugador(std::vector< std::vector< unsigned char> > mapaR) : Comportamiento(mapaR) {
-      // Inicializar Variables de Estado
-      hayPlan = false;
-      tieneBikini = false;
-      tieneZapatillas = false;
-    }
-    ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
-    ~ComportamientoJugador(){}
+class ComportamientoJugador : public Comportamiento
+{
+public:
+  ComportamientoJugador(unsigned int size) : Comportamiento(size)
+  {
+    hayPlan = false;
+    bikini = false;
+    zapatillas = false;
+  }
+  ComportamientoJugador(std::vector<std::vector<unsigned char>> mapaR) : Comportamiento(mapaR)
+  {
+    hayPlan = false;
+    bikini = false;
+    zapatillas = false;
+  }
+  ComportamientoJugador(const ComportamientoJugador &comport) : Comportamiento(comport) {}
+  ~ComportamientoJugador() {}
 
-    Action think(Sensores sensores);
-    int interact(Action accion, int valor);
-    void VisualizaPlan(const estado &st, const list<Action> &plan);
-    ComportamientoJugador * clone(){return new ComportamientoJugador(*this);}
+  Action think(Sensores sensores);
+  int interact(Action accion, int valor);
+  void VisualizaPlan(const estado &st, const list<Action> &plan);
+  ComportamientoJugador *clone() { return new ComportamientoJugador(*this); }
 
-  private:
-    // Declarar Variables de Estado
-    estado actual;
-    list<estado> objetivos;
-    list<Action> plan;
-    bool hayPlan;
-    bool tieneBikini;
-    bool tieneZapatillas;
+private:
+  estado actual;
+  list<estado> objetivos;
+  list<Action> plan;
 
-    // Métodos privados de la clase
-    bool pathFinding(int level, const estado &origen, const list<estado> &destino, list<Action> &plan);
-    bool pathFinding_Profundidad(const estado &origen, const estado &destino, list<Action> &plan);
-    bool pathFinding_Anchura(const estado &origen, const estado &destino, list<Action> &plan);
-    bool pathFinding_Coste_Uniforme(const estado &origen, const estado &destino, list<Action> &plan);
+  //Variables de estado añadidas
+  bool hayPlan;
+  bool bikini;
+  bool zapatillas;
 
-    void PintaPlan(list<Action> plan);
-    bool HayObstaculoDelante(estado &st);
-    void CalcularCosteCasillaAvanzar(estado &st, const vector<vector<unsigned char>> &map);
-    void CalcularCosteCasillaGirar(estado &st, const vector<vector<unsigned char>> &map);
+  bool pathFinding(int level, const estado &origen, const list<estado> &destino, list<Action> &plan);
+  bool pathFinding_Profundidad(const estado &origen, const estado &destino, list<Action> &plan);
+  bool pathFinding_Anchura(const estado &origen, const estado &destino, list<Action> &plan);
+  bool pathFinding_Coste_Uniforme(const estado &origen, const estado &destino, list<Action> &plan);
 
-
+  void PintaPlan(list<Action> plan);
+  bool HayObstaculoDelante(estado &st);
+  void CalcularCosteCasillaAvanzar(estado &st, const vector<vector<unsigned char>> &map);
+  void CalcularCosteCasillaGirar(estado &st, const vector<vector<unsigned char>> &map);
 };
 
 #endif
